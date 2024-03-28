84b236c3d29bb3114f661793a96f108284a2e4a3
banana <banana@tensor.so>
2024-03-28 11:33:46 -0400

    save state

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ import { Fragment, useState } from "react";
 
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Do I need to register first before submitting a project?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "",
   },
   {
     question: "What's the best thing about Switzerland?",
@@ -151,14 +151,14 @@ export default function Home() {
               <div className="items-center hidden md:flex bg-gray-800 bg-opacity-65 rounded-full my-4 py-4 px-8">
                 <a
                   href="https://tensor.readme.io"
-                  className="mx-3 text-lg text-white uppercase cursor-pointer hover:text-gray-300"
+                  className="mx-3 text-lg text-gray-300 uppercase cursor-pointer hover:text-white"
                 >
                   Resources
                 </a>
-                <a className="mx-3 text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+                <a className="mx-3 text-lg text-gray-300 uppercase cursor-pointer hover:text-white">
                   Register Now
                 </a>
-                <a className="mx-3 text-lg text-white uppercase cursor-pointer hover:text-gray-300">
+                <a className="mx-3 text-lg text-gray-300 uppercase cursor-pointer hover:text-white">
                   F.A.Q.
                 </a>
               </div>
@@ -175,7 +175,7 @@ export default function Home() {
             </h2>
             <a
               href="#"
-              className="block px-4 py-3 mt-10 text-lg font-bold text-white uppercase bg-gray-800 hover:bg-gray-900 register-button"
+              className="block px-4 py-3 mt-10 text-lg font-bold text-white uppercase rounded-lg  bg-gray-800 hover:bg-gray-900 register-button"
             >
               Register Now
             </a>
@@ -226,6 +226,7 @@ export default function Home() {
             <div
               className="relative flex items-center text-center space-x-3 rounded-lg border border-gray-300 bg-black px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400 anaglyphCard"
               key={Math.random()}
+              onClick={() => setOpen(true)}
             >
               <div className="flex-shrink-0">
                 {/* <img
@@ -235,7 +236,7 @@ export default function Home() {
               /> */}
               </div>
               <div className="min-w-0 flex-1">
-                <a href="#" className="focus:outline-none">
+                <a href="#" className="focus:outline-none" onClick={(e)=>{e.preventDefault()}}>
                   <span className="absolute inset-0" aria-hidden="true"></span>
                   <p className="font-medium text-gray-200">Tokenized Assets</p>
                   <p className="truncate text-gray-500">Sponsored by You</p>
@@ -256,7 +257,7 @@ export default function Home() {
           id="judgeList"
           className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-10 my-10"
         >
-          {judges.map((judge) => (
+          {judges.map((judge,i) => (
             <li
               className="col-span-1 flex flex-col rounded-lg bg-black text-center shadow anaglyphCard text-white"
               key={Math.random()}
@@ -264,7 +265,7 @@ export default function Home() {
               <div className="flex flex-1 flex-col p-8">
                 <img
                   className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
-                  src={judge.image}
+                  src={`/images/judges/${(i%8)+1}.png`}
                   alt=""
                 />
                 <h3 className="mt-6 text-sm font-medium text-gray-200">name</h3>
@@ -278,130 +279,129 @@ export default function Home() {
           ))}
         </ul>
       </section>
-
-      <section>
-        <div className="">
-          <h1 className="mt-4 font-extrabold leading-tight text-center text-white text-7xl sm:text-8xl anaglyph">
-            FAQ
-          </h1>
-          <div className="mx-auto max-w-7xl px-6 py-14">
-            <div className="mx-auto max-w-4xl divide-y divide-white/10">
-              <dl className="space-y-6 divide-y divide-white/10">
-                {faqs.map((faq) => (
-                  <Disclosure as="div" key={faq.question} className="pt-6">
-                    {({ open }) => (
-                      <>
-                        <dt className="">
-                          <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
-                            <span className="text-base font-semibold mt-6 leading-7">
-                              {faq.question}
-                            </span>
-                            <span className="ml-6 flex h-7 items-center">
-                              {open ? (
-                                <MinusSmallIcon
-                                  className="h-6 w-6"
-                                  aria-hidden="true"
-                                />
-                              ) : (
-                                <PlusSmallIcon
-                                  className="h-6 w-6"
-                                  aria-hidden="true"
-                                />
-                              )}
-                            </span>
-                          </Disclosure.Button>
-                        </dt>
-                        <Disclosure.Panel as="dd" className="mt-4 pr-12">
-                          <p className="text-base leading-7 text-gray-300">
-                            {faq.answer}
-                          </p>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                ))}
-              </dl>
-            </div>
+      <div className="relative  overflow-hidden bg-indigo-900">
+        <div className="image-container">
+          <img
+            src="/images/blobs.jpg"
+            className="absolute object-cover w-full h-full grayscale"
+          />
+          <div className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t h-1/2 from-transparent to-black">
+            <h1>something</h1>
           </div>
-        </div>
-      </section>
-
-      <footer className="bg-black mt-12">
-        <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8 z-10">
-          <nav
-            className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-            aria-label="Footer"
-          >
-            <div className="pb-6">
-              <a
-                href="#"
-                className="text-sm leading-6 text-gray-200 hover:text-gray-900"
-              >
-                About
-              </a>
-            </div>
-            <div className="pb-6">
-              <a
-                href="#"
-                className="text-sm leading-6 text-gray-200 hover:text-gray-900"
-              >
-                Blog
-              </a>
-            </div>
-            <div className="pb-6">
-              <a
-                href="#"
-                className="text-sm leading-6 text-gray-200 hover:text-gray-900"
-              >
-                Jobs
-              </a>
-            </div>
-          </nav>
-          <div className="mt-10 flex justify-center space-x-10">
-            <a href="#" className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">X</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-              </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">GitHub</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </div>
-          <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-            &copy; 2024 Tensor Labs. All rights reserved.
-          </p>
-        </div>
-      </footer>
-      <div
-        className="relative"
-        style={{
-          backgroundImage: "url('/images/blobsB.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="absolute inset-0 bg-black opacity-25">
-          &nbsp;<h1>hi</h1>
         </div>
       </div>
+
+      <section className="">
+        
+        <h1 className="mt-4 font-extrabold leading-tight text-center text-white text-7xl sm:text-8xl anaglyph">
+          FAQ
+        </h1>
+        <div className="mx-auto max-w-7xl px-6 py-14">
+          <div className="mx-auto max-w-4xl divide-y divide-white/10">
+            <dl className="space-y-6 divide-y divide-white/10">
+              {faqs.map((faq) => (
+                <Disclosure as="div" key={faq.question} className="pt-6">
+                  {({ open }) => (
+                    <>
+                      <dt className="">
+                        <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
+                          <span className="text-base font-semibold mt-6 leading-7">
+                            {faq.question}
+                          </span>
+                          <span className="ml-6 flex h-7 items-center">
+                            {open ? (
+                              <MinusSmallIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <PlusSmallIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            )}
+                          </span>
+                        </Disclosure.Button>
+                      </dt>
+                      <Disclosure.Panel as="dd" className="mt-4 pr-12">
+                        <p className="text-base leading-7 text-gray-300">
+                          {faq.answer}
+                        </p>
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              ))}
+            </dl>
+          </div>
+        
+      </div>
+    </section>
+
+    <footer className="bg-black mt-12">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8 z-10">
+        <nav
+          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+          aria-label="Footer"
+        >
+          <div className="pb-6">
+            <a
+              href="#"
+              className="text-sm leading-6 text-gray-200 hover:text-gray-900"
+            >
+              About
+            </a>
+          </div>
+          <div className="pb-6">
+            <a
+              href="#"
+              className="text-sm leading-6 text-gray-200 hover:text-gray-900"
+            >
+              Blog
+            </a>
+          </div>
+          <div className="pb-6">
+            <a
+              href="#"
+              className="text-sm leading-6 text-gray-200 hover:text-gray-900"
+            >
+              Jobs
+            </a>
+          </div>
+        </nav>
+        <div className="mt-10 flex justify-center space-x-10">
+          <a href="#" className="text-gray-400 hover:text-gray-500">
+            <span className="sr-only">X</span>
+            <svg
+              className="h-6 w-6"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+            </svg>
+          </a>
+          <a href="#" className="text-gray-400 hover:text-gray-500">
+            <span className="sr-only">GitHub</span>
+            <svg
+              className="h-6 w-6"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          &copy; 2024 Tensor Labs. All rights reserved.
+        </p>
+      </div>
+    </footer>
     </>
   );
 }
